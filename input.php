b935e5ede09f9b1508f6a8aa6c5d4aa5870a2e37
Chris Johnson <chrisjohnson112@yahoo.com>
2017-06-30 11:16:58 -0500

    initial upload

---
+++ input.php
@@ -0,0 +1,32 @@
+<?php 
+//Only processif form isn't empty
+
+ 	   class MyDB extends SQLite3
+   {
+      function __construct()
+      {
+         $this->open('db/test.db');
+      }
+   }
+   $db = new MyDB();
+
+   $name = $_POST['name'];
+ 
+
+ 	//insert our data
+ 	$sql = "INSERT INTO employees (name) VALUES ('$name')";
+ 	$insert = $db->query($sql);
+
+ 	//print response from mySQL
+ 	/*if($insert) {
+ 		echo "Success! Row ID: {$mysqli->insert_id}";
+ 	} else {
+ 		die("Error: {mysqli->errno} : {$mysqli->error}");
+ 	}*/
+
+ 	//close connection
+ 	
+
+
+?>
+header("Name entered");
